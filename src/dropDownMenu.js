7125496b248b4134f9f274e0eefc551c20e48f35
marin0suke <monk.marina@gmail.com>
2024-11-08 12:21:30 +1000

    Add hardcoded HTML and CSS styling for menu items.

--- src/dropDownMenu.js
+++ src/dropDownMenu.js
@@ -1,6 +1,19 @@
 
-function dropDownMenu() {
+export function dropDownMenu() {
+    //logic for drop down here.
+    const menu = document.querySelector(".drop-down-button");
+    menu.addEventListener("click", () => {
+        const menuItems = document.querySelector(".menu-items");
+
+        toggleDropDown
+    })
     
+    // set up event listener for the menu.
+
 };
 
-export default dropDownMenu;
+//toggle function for button. 
+
+function toggleDropDown(element) {
+    return element.display
+}
